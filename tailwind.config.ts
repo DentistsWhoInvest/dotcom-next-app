a37dbb1367a0855cc1be928daaf296d6801c6677
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-09-07 22:11:49 +0100

    Add shadcn tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,35 +2,42 @@ import type { Config } from "tailwindcss";
 const colors = require('tailwindcss/colors')
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'blue-primary': '#151f6d',
-      'blue-secondary': '#65AEC1',
-      'blue-light': '#7FE1FA',
-      'grey-primary': '#333f48',
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      orange: colors.orange,
-    },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      fontFamily: {sans: ['Poppins', 'sans-serif']}
-    },
+  	colors: {
+  		'blue-primary': '#151f6d',
+  		'blue-secondary': '#65AEC1',
+  		'blue-light': '#7FE1FA',
+  		'grey-primary': '#333f48',
+  		transparent: 'transparent',
+  		current: 'currentColor',
+  		black: colors.black,
+  		white: colors.white,
+  		gray: colors.gray,
+  		emerald: colors.emerald,
+  		indigo: colors.indigo,
+  		orange: colors.orange
+  	},
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		fontFamily: {
+  			sans: ['Poppins', 'sans-serif']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
