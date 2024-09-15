04c9b238249716c292b4a7d567ab94f2ecb1e8aa
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-09-15 14:30:11 +0100

    Tweak fetch function to support paginated requests

--- src/lib/fetchUtils.ts
+++ src/lib/fetchUtils.ts
@@ -1,10 +1,10 @@
 import { signJwt } from "./signJwt";
 
 /**
- * TODO: figure out something about the return type... specify in each page? 
- * @param requestedEndpoint 
- * @returns 
- * Can be used in getStaticProps: 
+ * TODO: figure out something about the return type... specify in each page?
+ * @param requestedEndpoint
+ * @returns
+ * Can be used in getStaticProps:
  * export const getStaticProps = async () => {
  *   const pageData = await fetchEndpointData('/about');
  *   return {
@@ -14,7 +14,17 @@ import { signJwt } from "./signJwt";
  *   };
  *   };
  */
-export async function fetchEndpointData(requestedEndpoint: string, populateFields: string[] = ["*"]) {
+
+type PaginationOptions = {
+  makingPaginatedRequest: boolean;
+  paginatedQueryParams: { page: number; pageSize: number };
+};
+
+export async function fetchEndpointData(
+  requestedEndpoint: string,
+  populateFields: string[] = ["*"],
+  paginationOptions?: PaginationOptions
+) {
   const endpoint = `${process.env.NEXT_ADMIN_STRAPI_URL}${requestedEndpoint}`;
   let headers = {
     Authorization: `bearer ${process.env.STRAPI_API_KEY}`,
@@ -25,7 +35,17 @@ export async function fetchEndpointData(requestedEndpoint: string, populateField
     let jwt = await signJwt(endpoint);
     headers["Proxy-Authorization"] = `Bearer ${jwt}`;
   }
-  const url: string = `${endpoint}?populate=${populateFields.join(",")}`;
+
+  function buildUrl(endpoint: string, paginationOptions?: PaginationOptions) {
+    const baseUrl: string = `${endpoint}?populate=${populateFields.join(",")}`;
+    if (paginationOptions?.makingPaginatedRequest) {
+      return `${baseUrl}&page=${paginationOptions.paginatedQueryParams.page}&pageSize=${paginationOptions.paginatedQueryParams.pageSize}`;
+    } else {
+      return baseUrl;
+    }
+  }
+  const url = buildUrl(endpoint, paginationOptions);
+
   const fetchRequest = await fetch(url, { method: "GET", headers });
   const resultJSON = await fetchRequest.json();
   return resultJSON;
