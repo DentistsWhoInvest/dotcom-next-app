834cbdef638e59099eb3c2b784c3f0278c028879
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-09-30 01:03:26 +0100

    Full privacy policy page, wip tandc

--- src/pages/terms-and-conditions.tsx
+++ src/pages/terms-and-conditions.tsx
@@ -1,3 +1,13 @@
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+
+type TAndCAttributes = {
+  terms: any;
+  last_updated: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+};
+
 export const getStaticProps = async () => {
   const testResult = [
     {
@@ -18,13 +28,24 @@ export const getStaticProps = async () => {
 
 export default function TermsAndConditions({ result }: { result: any[] }) {
   return (
-    <main className={`flex flex-col`}>
-      <div>
-        <div>
-          {result[0].attributes.title}
-          {result[0].attributes.description}
-        </div>
+    <main
+      className={`flex flex-col shadow-[rgba(0,0,15,0.5)_0px_0px_15px_0px] `}
+    >
+      <div
+        id="banner"
+        className="flex h-[200px] items-center justify-center bg-[#dbe2e9] text-center text-[45px] font-bold text-blue-primary "
+      >
+        Terms and Conditions
       </div>
+      {/* <div className="t-and-c my-8 space-y-8 px-6 pt-8 lg:m-6 xl:mx-36">
+        <BlocksRenderer content={policyPage.policy} />
+        <h4>Attribution</h4>
+        <BlocksRenderer content={policyPage.attribution} />
+        <span>
+          This privacy policy was created on{" "}
+          <span className="font-bold">{formattedDate}</span>.
+        </span>
+      </div> */}
     </main>
   );
 }
