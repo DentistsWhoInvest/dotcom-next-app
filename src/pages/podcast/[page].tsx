b7d24a66232cb9bb247b4237a1a2234c05b38e63
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-10-13 21:55:27 +0100

    Add missing key

--- src/pages/podcast/[page].tsx
+++ src/pages/podcast/[page].tsx
@@ -202,7 +202,7 @@ export default function Podcasts({ pageData, currentPage, totalPages }: { pageDa
       <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 self-center">
         {sortedData.map((page: Episode) => {
           return (
-            <Link href={`/e${page.attributes.episode_number}`}>
+            <Link href={`/e${page.attributes.episode_number}`} key={page.id}>
               <div className="m-6 justify-evenly border-2 border-blue-secondary shadow-custom bg-white rounded-2xl w-[315px] text-center flex flex-col lg:w-[430px]">
                 <Image
                   src={page.attributes.artwork_url}
