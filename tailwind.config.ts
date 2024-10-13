10ae969c7f4bd190d473bf5ca19e9f3fc313b978
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-10-13 13:31:37 +0100

    Cash flow page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,7 @@ const config: Config = {
   	extend: {
 		boxShadow: {
 			'custom': '0px 0px 10px 0px rgba(0, 0, 0, 0.5)',
+			'custom-br': '15px 15px 25px -10px rgba(0,0,0,0.5)',
 		  },
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
