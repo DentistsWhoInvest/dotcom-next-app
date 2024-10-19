9ca54711109889598c417f89d322436284d70bd3
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-10-19 18:49:49 +0100

    add type to marketing landing page

--- src/pages/[targetedlandingpage].tsx
+++ src/pages/[targetedlandingpage].tsx
@@ -1,22 +1,64 @@
 import { fetchEndpointData } from "@/lib/fetchUtils";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 
+type TextNode = {
+  text: string;
+  type: string;
+};
+
+type Paragraph = {
+  type: "paragraph";
+  children: TextNode[];
+};
+
+type Description = Paragraph[];
+
+type Contributor = {
+  data: null | {
+    id: number;
+    attributes: {
+      firstName: string;
+      lastName: string;
+      createdAt: string;
+      updatedAt: string;
+      title: string | null;
+    };
+  };
+};
+
+type LandingPageAttributes = {
+  title: string;
+  description: Description;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  contributor: Contributor;
+};
+
+type LandingPage = {
+  id: number;
+  attributes: LandingPageAttributes;
+};
+
 export const getStaticPaths = async () => {
-  const results: any = await fetchEndpointData(`/targeted-data-collection-page`);
+  const results: any = await fetchEndpointData(
+    `/targeted-data-collection-pages`
+  );
+  console.log("results", results);
   return {
     paths: results.data.map((result: { id: number }) => ({
       params: { targetedlandingpage: result.id.toString() },
     })),
     fallback: false,
   };
-}
+};
 
 export const getStaticProps = async () => {
-  const pageData = await fetchEndpointData("/targeted-data-collection-page/5");
+  const pageData = await fetchEndpointData("/targeted-data-collection-pages/5");
 
   return {
     props: {
-      landingPage: pageData.data.attributes,
+      landingPage: pageData.data,
     },
   };
 };
@@ -24,16 +66,10 @@ export const getStaticProps = async () => {
 export default function TargetedMarketingLandingPage({
   landingPage,
 }: {
-  landingPage: any;
+  landingPage: LandingPage;
 }) {
-  const formattedDate = new Date(landingPage.last_updated).toLocaleDateString(
-    "en-US",
-    {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    }
-  );
+  console.log("landingPage", landingPage);
+
   return (
     <main
       className={`flex flex-col shadow-[rgba(0,0,15,0.5)_0px_0px_15px_0px] `}
@@ -42,14 +78,11 @@ export default function TargetedMarketingLandingPage({
         id="banner"
         className="flex h-[200px] items-center justify-center bg-[#dbe2e9] text-center text-[45px] font-bold text-blue-primary "
       >
-        Privacy Policy
+        marketing page
       </div>
       <div className="privacy-policy my-8 space-y-8 px-6 pt-8 lg:m-6 xl:mx-36">
         {/* <BlocksRenderer content={policyPage.policy} /> */}
-        <span>
-          This privacy policy was created on{" "}
-          <span className="font-bold">{formattedDate}</span>.
-        </span>
+        <span>test</span>
       </div>
     </main>
   );
