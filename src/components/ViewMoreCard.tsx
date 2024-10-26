a3df721d82c0321d9a1e13e38d8a20f1e2f5f198
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-10-26 20:41:29 +0100

    fix view more card spacing

--- src/components/ViewMoreCard.tsx
+++ src/components/ViewMoreCard.tsx
@@ -1,14 +1,6 @@
-import { Button } from "./ui/button";
 import { ChevronsRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import {
-  Card,
-  CardContent,
-  CardTitle,
-  CardDescription,
-  CardFooter,
-} from "./ui/card";
 import { processDate } from "@/lib/dateUtils";
 
 export const ViewMoreCard = ({
@@ -51,7 +43,7 @@ export const ViewMoreCard = ({
 
   return (
     <>
-      <div className="m-6 justify-evenly border-2 border-blue-secondary shadow-custom bg-white rounded-2xl w-[315px] text-center flex flex-col lg:w-[430px]">
+      <div className="m-6 justify-evenly border-2 border-blue-secondary shadow-custom bg-white rounded-2xl w-[315px] flex flex-col lg:w-[430px] h-[92%]">
         <Link href={`/${hrefStarter}/${slug}`}>
           <Image
             src={hrefStarter === 'blog-posts' ? page.attributes.cover.data.attributes.url : page.attributes.artwork_url}
@@ -61,20 +53,20 @@ export const ViewMoreCard = ({
             className="rounded-t-xl h-[218px] object-cover bg-blue-secondary border-blue-secondary border lg:w-[430px] lg:h-[300px]"
           />
         </Link>
-        <div className="text-center flex flex-col mx-8 my-4 grow space-y-4">
+        <div className="text-left flex flex-col mx-8 my-4 space-y-4 h-full">
           <p className="text-blue-primary text-[21px] font-bold">
-            <Link href={`/${hrefStarter}/${slug}`}>{page.attributes.title}</Link>
+            {/* <Link href={`/${hrefStarter}/${slug}`}>{page.attributes.title}</Link> */}
+            <Link href={`/${hrefStarter}/${slug}`} dangerouslySetInnerHTML={{__html:page.attributes.title}}></Link>
           </p>
-          {hrefStarter === 'blog-posts' && <div className="text-grey-primary text-base"
+          {hrefStarter === 'blog-posts' && <div className="text-grey-primary text-base grow"
                   key={page.attributes.excerpt}
                   dangerouslySetInnerHTML={{
                     __html:trimmedExcerpt
                     ,
                   }}
                 />}
-          <div className="grow"></div>
           <Link
-            className={"text-xs font-semibold text-blue-secondary"}
+            className="text-xs font-semibold text-blue-secondary "
             href={`/${hrefStarter}/${slug}`}
           >
             <span className="flex p-2">
