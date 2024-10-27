ed216a89de275ba990fa838d0128504d568ef854
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-10-27 03:27:49 +0000

    better hero banner text

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -56,46 +56,43 @@ export const HeroBanner = ({
   subText,
   podcastSubText = false,
 }: {
-  bannerImage: ImageAttributes | { url: string, name: string };
+  bannerImage: ImageAttributes | { url: string; name: string };
   bannerText: string;
   subText?: string;
   ctaButton?: ButtonData;
   podcastSubText?: boolean;
 }) => {
   return (
-    <div className="relative z-10 h-[430px] w-screen overflow-hidden md:h-[409px] xl:h-[570.75px]">
-        <Image
-          src={bannerImage.url}
-          alt={bannerImage.name}
-          layout="fill"
-          objectFit="cover"
-          className="inset-0 object-[right_70%] lg:object-[right_40%] xl:object-[right_30%] xxl:object-[right_25%]"
-        />
-        <div className="absolute inset-0 bg-blue-primary opacity-80 md:opacity-[0.69] xl:hidden"></div>
-
-      <div className="relative z-10 flex size-full flex-col items-center justify-center text-center md:max-w-[62%] md:items-start md:justify-center md:text-left lg:max-w-[50%] xl:mx-[130px] xl:max-w-[1140px] ">
-        <div className="absolute px-4 md:top-1/4 md:px-[30px] xl:top-[30%] xl:w-2/3">
-          <h1 className="mb-4 text-3xl font-bold text-white md:text-[35px] xl:mb-5 xl:text-[45px] xl:leading-[54px]">
-            {bannerText}
-          </h1>
-          {subText && (
-            <p className="text-[18px] leading-[21.6px] text-blue-light xl:text-xl">
-              {subText}
-            </p>
-          )}
-          {podcastSubText && (
-            <p className="text-[16px] leading-[21.6px] ">
-              <span className="text-orange-400">New episodes every: </span>
-              <br />
-              <span className="text-white">Monday | Wednesday | Friday</span>
-            </p>
-          )}
-          {ctaButton && (
-            <Button className="mt-8 rounded-md bg-orange-400 text-white hover:text-blue-primary ">
-              <Link href={ctaButton.url}>{ctaButton.text}</Link>
-            </Button>
-          )}
-        </div>
+    <div className="relative z-10 h-[430px] w-screen overflow-hidden md:h-[409px] lg:h-[570.75px]">
+      <Image
+        src={bannerImage.url}
+        alt={bannerImage.name}
+        layout="fill"
+        objectFit="cover"
+        className="inset-0 object-[right_70%] lg:object-[right_40%] xl:object-[right_30%] xxl:object-[right_25%]"
+      />
+      <div className="absolute inset-0 bg-blue-primary opacity-80 md:opacity-[0.69] lg:hidden"></div>
+      <div className="relative z-10 flex size-full flex-col items-center justify-center px-5 text-center md:w-[65%] md:max-w-screen-lg md:items-start md:px-[30px] md:text-left lg:mx-auto lg:w-[1140px] lg:grow lg:px-0">
+        <h1 className="-mt-4 mb-5 text-3xl font-bold text-white md:mb-6 md:mt-[-60px] md:text-[35px] lg:mb-5 lg:mt-[-12px] lg:w-1/2 lg:text-[45px] lg:leading-[54px]">
+          {bannerText}
+        </h1>
+        {subText && (
+          <p className="text-[18px] leading-[21.6px] text-blue-light md:leading-[25.2px] lg:w-1/2 lg:text-xl">
+            {subText}
+          </p>
+        )}
+        {podcastSubText && (
+          <p className="mt-10 text-[16px] leading-[21.6px] md:text-[18px] md:leading-[25.2px]">
+            <span className="text-orange-400">New episodes every: </span>
+            <br />
+            <span className="text-white">Monday | Wednesday | Friday</span>
+          </p>
+        )}
+        {ctaButton && (
+            <Button className="mt-8 rounded-md bg-orange-400 px-[55px] py-6 text-base text-white hover:bg-orange-500">
+            <Link href={ctaButton.url}>{ctaButton.text}</Link>
+          </Button>
+        )}
       </div>
     </div>
   );
