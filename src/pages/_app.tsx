8b66989b8f1a3bcbafc609656ad4e7a0c7b7d4a1
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-10-29 18:39:26 +0000

    Add free nhs checklist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,6 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
     "/thankyou",
     "/free-report",
     "/crypto-ebook",
+    "/free-nhs-checklist",
   ];
 
   const showHeaderAndFooter = !noHeaderOrFooterRoute.includes(router.pathname);
