cb9a0b3e9f5c95c774ba29df134609d7b976e357
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-10-29 21:52:53 +0000

    replace form on articles

--- src/components/NHSPensionChecklistForm.tsx
+++ src/components/NHSPensionChecklistForm.tsx
@@ -3,17 +3,16 @@ import Script from "next/script";
 export default function NHSPensionChecklistForm() {
   return (
     <section className="bg-orange-400 text-white xl:w-[330px]">
-      <div className="mb-4 text-center text-2xl font-bold xl:text-4xl md:text-left p-[20px]">
-        Have you got your NHS Pension checklist yet?
+      <div className="mb-[-50px] p-[20px] text-center text-[25px] font-bold md:text-left lg:text-[33px] lg:leading-[39px]">
+      FREE pdf on tax efficiency for dentists
       </div>
       <div
         // eslint-disable-next-line tailwindcss/no-custom-classname
-        className="_form_26"
+        className="_form_23"
       ></div>
-      <Script src="https://dentistswhoinvest.activehosted.com/f/embed.php?id=26" />
-      <p className="pt-4 text-[18px] leading-5 md:text-left lg:text-base px-[20px]">
-        Enter your details above to receive a link you can use to download your
-        FREE checklist
+      <Script src="https://dentistswhoinvest.activehosted.com/f/embed.php?id=23" />
+      <p className="pb-4 text-[12px] leading-5 md:text-left px-[20px] mt-[-10px]">
+      Enter your details above to receive a link you can use to download your FREE pdf
       </p>
     </section>
   );
