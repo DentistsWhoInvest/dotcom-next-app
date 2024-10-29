7c72a0a10d9aca5c9b12654365c71771ee0d78f3
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-10-29 18:45:59 +0000

    Setup subscribe to newsletter page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,6 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
     "/free-report",
     "/crypto-ebook",
     "/free-nhs-checklist",
+    "/subscribe-to-our-newsletter",
   ];
 
   const showHeaderAndFooter = !noHeaderOrFooterRoute.includes(router.pathname);
