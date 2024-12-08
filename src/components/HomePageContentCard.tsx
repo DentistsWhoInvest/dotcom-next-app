2a5de4a697099bfa86abf389da9baefc8d5297c5
Georges Lebreton <georges.a.lebreton@gmail.com>
2024-12-08 20:51:55 +0000

    adjust splash text/headline size if podcast/video

--- src/components/HomePageContentCard.tsx
+++ src/components/HomePageContentCard.tsx
@@ -53,7 +53,9 @@ const SplashCard = ({
       <div className="flex flex-col items-start p-4 lg:w-1/3">
         <Link
           href={url}
-          className="pb-4 text-lg font-semibold text-blue-primary"
+          className={`pb-4 font-semibold text-blue-primary ${
+            description ? "text-lg" : "text-2xl"
+          }`}
         >
           {title}
         </Link>
@@ -69,9 +71,12 @@ const SplashCard = ({
         )}
         <Link
           href={url}
-          className="pt-2 text-sm font-semibold text-blue-primary"
+          className={` text-sm font-semibold text-blue-primary 
+            ${description ? "pt-2" : "pt-4"}`}
         >
-          Read More...
+          {type === "article" && "Read more..."}
+          {type === "podcast" && "Listen here"}
+          {type === "video" && "Watch here"}
         </Link>
       </div>
     </div>
@@ -96,7 +101,7 @@ const LargeCard = ({ title, type, url, imageUrl, imageAlt }: CardProps) => {
           />
         </Link>
       </div>
-      <div className="flex flex-col items-start px-4 py-2 grow h-2/5">
+      <div className="flex flex-col items-start lg:px-4 px-2 py-2 grow h-2/5">
         <p className="mb-2 bg-blue-primary px-3 text-[10px] font-semibold uppercase text-white">
           {type}
         </p>
@@ -114,7 +119,7 @@ const MediumCard = ({ title, type, url, imageUrl, imageAlt }: CardProps) => {
       id={title}
       className="flex flex-row overflow-hidden bg-white shadow-lg lg:flex-col lg:max-h-[320px]"
     >
-      <div className="relative w-1/4 lg:w-full aspect-square max-h-full overflow-hidden">
+      <div className="relative w-2/5 lg:w-full aspect-square max-h-full overflow-hidden">
         <Link href={url}>
           <Image
             src={imageUrl}
