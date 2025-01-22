c32e847923b9c858aa10ee5c00196d4624c6e127
Georges Lebreton <georges.a.lebreton@gmail.com>
2025-01-22 21:28:31 +0000

    add placeholder party poppers on congrats page

--- src/components/CPDPagesHeader.tsx
+++ src/components/CPDPagesHeader.tsx
@@ -1,9 +1,25 @@
 export default function CPDPagesHeader({ title }: { title: string }) {
+  const isCongratulations = title === "Congratulations";
+
   return (
-    <div className="flex flex-col items-center bg-blue-primary text-white">
-      {title}
-      {title === "Congratulations" && (
-        <p>You have successfully completed this CPD/CE</p>
+    <div className="flex h-[60px] items-center justify-center bg-gradient-to-r from-blue-primary to-blue-secondary text-white md:h-[100px]">
+      {isCongratulations && (
+        <img
+          className="w-10 h-10 mr-4"
+          src="https://static.vecteezy.com/system/resources/previews/006/600/009/non_2x/exploding-party-poppers-with-confetti-free-vector.jpg"
+        />
+      )}
+      <div className="flex-col">
+        <p className="text-xl font-semibold">{title}</p>
+        {isCongratulations && (
+          <p className="pt-2">You have successfully completed this CPD/CE</p>
+        )}
+      </div>
+      {isCongratulations && (
+        <img
+          className="w-10 h-10 ml-4"
+          src="https://static.vecteezy.com/system/resources/previews/006/600/009/non_2x/exploding-party-poppers-with-confetti-free-vector.jpg"
+        />
       )}
     </div>
   );
