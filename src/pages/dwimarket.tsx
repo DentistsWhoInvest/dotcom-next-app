d4f513a09ae947e228bdc5ae108ef59559e32b94
Georges Lebreton <georges.a.lebreton@gmail.com>
2025-02-23 14:57:56 +0000

    tweak desktop dwimarket page

--- src/pages/dwimarket.tsx
+++ src/pages/dwimarket.tsx
@@ -25,23 +25,23 @@ export const getStaticProps = async () => {
 export default function StoreIntro({ introCopy, ctaCopy, introVideo }: StoreIntroProps) {
   return (
       <main className={`w-screen min-h-screen bg-gradient-to-b from-blue-secondary to-blue-primary flex flex-col justify-center`}>
-        <section className="p-[20px] size-full flex flex-col content-center place-content-center items-center justify-items-center xl:max-w-[1440px] xl:mx-auto">
+        <section className="p-[20px] size-full flex flex-col content-center place-content-center items-center justify-items-center xl:max-w-[1024px] xl:mx-auto">
           <div className="text-center  text-white">
-            <h1 className="text-3xl md:text-5xl xl:text-6xl font-bold">{introCopy[0].children[0].text}</h1>
-            <h2 className="text-2xl md:text-4xl xl:text-5xl" style={{ marginTop: '20px', marginBottom: '20px' }}>{introCopy[1].children[0].text}</h2>
+            <h1 className="text-3xl md:text-4xl xl:text-5xl font-bold">{introCopy[0].children[0].text}</h1>
+            <h2 className="text-2xl md:text-3xl lg:text-4xl" style={{ marginTop: '20px', marginBottom: '20px' }}>{introCopy[1].children[0].text}</h2>
             <p className="text-lg md:text-xl xl:text-2xl mt-5 mb-5" style={{ marginTop: '20px', marginBottom: '20px' }}>{introCopy[2].children[0].text}</p>
           </div>
           
           {/* Video Player if Available */}
           {introVideo && (
-            <video controls className="w-full max-w-5xl mx-auto mb-6" poster="https://assets.dentistswhoinvest.com/manually_managed_assets/intro-poster.webp">
+            <video controls className="w-full max-w-[60rem] mx-auto mb-8" poster="https://assets.dentistswhoinvest.com/manually_managed_assets/intro-poster.webp">
               <source src={introVideo.data.attributes.url} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           )}
 
           <Link href={"dwimarket-tutorial"} passHref>
-            <button className="bg-orange-400 py-[10px] px-[30px] text-[25px] md:text-[45px] text-white">
+            <button className="bg-orange-400 py-[8px] px-[30px] text-[25px] md:text-[35px] text-white">
               {ctaCopy}
             </button>
           </Link>
