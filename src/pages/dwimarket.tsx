406c5c25b2293bb443c935f110dbeaef3d8c468b
Georges Lebreton <georges.a.lebreton@gmail.com>
2025-02-23 14:23:49 +0000

    tweak padding on button and text size on lg breakpoint

--- src/pages/dwimarket.tsx
+++ src/pages/dwimarket.tsx
@@ -27,9 +27,9 @@ export default function StoreIntro({ introCopy, ctaCopy, introVideo }: StoreIntr
       <main className={`w-screen min-h-screen bg-gradient-to-b from-blue-secondary to-blue-primary flex flex-col justify-center`}>
         <section className="p-[20px] size-full flex flex-col content-center place-content-center items-center justify-items-center xl:max-w-[1440px] xl:mx-auto">
           <div className="text-center  text-white">
-            <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold">{introCopy[0].children[0].text}</h1>
-            <h2 className="text-2xl md:text-4xl lg:text-5xl" style={{ marginTop: '20px', marginBottom: '20px' }}>{introCopy[1].children[0].text}</h2>
-            <p className="text-lg md:text-xl lg:text-2xl mt-5 mb-5" style={{ marginTop: '20px', marginBottom: '20px' }}>{introCopy[2].children[0].text}</p>
+            <h1 className="text-3xl md:text-5xl xl:text-6xl font-bold">{introCopy[0].children[0].text}</h1>
+            <h2 className="text-2xl md:text-4xl xl:text-5xl" style={{ marginTop: '20px', marginBottom: '20px' }}>{introCopy[1].children[0].text}</h2>
+            <p className="text-lg md:text-xl xl:text-2xl mt-5 mb-5" style={{ marginTop: '20px', marginBottom: '20px' }}>{introCopy[2].children[0].text}</p>
           </div>
           
           {/* Video Player if Available */}
@@ -41,7 +41,7 @@ export default function StoreIntro({ introCopy, ctaCopy, introVideo }: StoreIntr
           )}
 
           <Link href={"dwimarket-tutorial"} passHref>
-            <button className="bg-orange-400 py-[15px] px-[30px] text-[25px] md:text-[45px] text-white">
+            <button className="bg-orange-400 py-[10px] px-[30px] text-[25px] md:text-[45px] text-white">
               {ctaCopy}
             </button>
           </Link>
