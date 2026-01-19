f2bba92709b9f3b91ab961f0aa929eee88345d33
Georges Lebreton <georges.a.lebreton@gmail.com>
2026-01-19 02:06:03 +0000

    restore persist middleware

--- src/stores/quizStore.ts
+++ src/stores/quizStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type QuizState = {
   selectedAnswers: Record<number, number>;
@@ -15,44 +16,49 @@ type QuizState = {
 };
 
 export const useQuizStore = create<QuizState>()(
-  (set) => ({
-    selectedAnswers: {},
-    setAnswer: (questionId, answerId) =>
-      set((state) => ({
-        selectedAnswers: { ...state.selectedAnswers, [questionId]: answerId },
-      })),
-    resetAnswers: () => set({ selectedAnswers: {} }),
-    
-    reflectionAnswers: {},
-    
-    setReflectionAnswers: (quizId, questionId, question, answer) =>
-      set((state) => {
-        const quizAnswers = state.reflectionAnswers[quizId] || [];
-        const index = quizAnswers.findIndex(
-          (item) => item.questionId === questionId
-        );
-        
-        let updatedQuizAnswers;
-        if (index >= 0) {
-          updatedQuizAnswers = [...quizAnswers];
-          updatedQuizAnswers[index] = { questionId, question, answer };
-        } else {
-          updatedQuizAnswers = [...quizAnswers, { questionId, question, answer }];
-        }
-        
-        return {
-          reflectionAnswers: {
-            ...state.reflectionAnswers,
-            [quizId]: updatedQuizAnswers,
-          },
-        };
-      }),
-    
-    resetReflectionAnswers: (quizId) =>
-      set((state) => {
-        const updated = { ...state.reflectionAnswers };
-        delete updated[quizId];
-        return { reflectionAnswers: updated };
-      }),
-  }),
+  persist(
+    (set) => ({
+      selectedAnswers: {},
+      setAnswer: (questionId, answerId) =>
+        set((state) => ({
+          selectedAnswers: { ...state.selectedAnswers, [questionId]: answerId },
+        })),
+      resetAnswers: () => set({ selectedAnswers: {} }),
+
+      reflectionAnswers: {},
+
+      setReflectionAnswers: (quizId, questionId, question, answer) =>
+        set((state) => {
+          const quizAnswers = state.reflectionAnswers[quizId] || [];
+          const index = quizAnswers.findIndex(
+            (item) => item.questionId === questionId
+          );
+
+          let updatedQuizAnswers;
+          if (index >= 0) {
+            updatedQuizAnswers = [...quizAnswers];
+            updatedQuizAnswers[index] = { questionId, question, answer };
+          } else {
+            updatedQuizAnswers = [...quizAnswers, { questionId, question, answer }];
+          }
+
+          return {
+            reflectionAnswers: {
+              ...state.reflectionAnswers,
+              [quizId]: updatedQuizAnswers,
+            },
+          };
+        }),
+
+      resetReflectionAnswers: (quizId) =>
+        set((state) => {
+          const updated = { ...state.reflectionAnswers };
+          delete updated[quizId];
+          return { reflectionAnswers: updated };
+        }),
+    }),
+    {
+      name: 'quiz-storage', // unique name for localStorage key
+    }
+  )
 );
